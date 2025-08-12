96a9aebc44c525d3fab32ab8d23aa71c32964d9a
EsakkiDevi <149249685+EsakkiDevi@users.noreply.github.com>
2025-08-12 09:29:39 +0530

    Assignments with full completion

--- Assignment/Assignment_day12.java
+++ Assignment/Assignment_day12.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 class MyThread extends Thread
 	{
 		public void run()
@@ -76,21 +77,328 @@ class MainTaskrunner
 		}
 	}
 //---------------------------------------------------------------------------------------
-
-class oddeven implements Runnable
+class prints
 	{
+		int n=20;
+		int i=1;
+		public synchronized void printodd()
+		{
+			while(i<=n)
+			{
+			if(i%2==1)
+				{
+					System.out.println("Odd:"+i);
+					i++;
+					notify();
+				}
+			else
+				{
+					try{
+						wait();
+					}
+					catch(Exception e)
+					{
+						e.printStackTrace();
+					}
+				}
+
+			}
+		}
+		
+		public synchronized void printeven()
+		{
+			while(i<=n)
+			{
+			if(i%2==0)
+				{
+					System.out.println("Even:"+i);
+					i++;
+					notify();
+				}
+			else
+				{
+					try{
+						wait();
+					}
+					catch(Exception e)
+					{
+						e.printStackTrace();
+					}
+				}
+
+			}
+		}
+
+	}
+class oddeven
+	{
+		public static void main(String args[])
+		{
+			prints p=new prints();
+			thread1 t1=new thread1(p);
+			thread2 t2=new thread2(p);
+			t1.start();
+			t2.start();
+		
+		}
+	}
+class thread1 extends Thread
+{
+	prints p;
+	
+	thread1(prints p)
+	{
+	this.p=p;
+	}
+	
+	
+	public void run()
+		{
+			p.printodd();
+		}
+}
+class thread2 extends Thread
+{
+	prints p;
+	
+	thread2(prints p)
+	{
+	this.p=p;
+	}
+	public void run()
+		{
+			p.printeven();
+		}
+}
+//--------------------------------------------------------------------------
+class threadcount extends Thread
+	{
+		counting c;
+		threadcount(counting c)
+		{
+			this.c=c;
+		}
 		public void run()
 		{
-			try
+			for(int i=0;i<1000;i++)
 			{
-				for(int i=1;i<=20;i++)
-				{
-					if(i%2==1)
-					{
-					System.out.println("Thread "+name+" print odd number "
+			
+			c.countincrement();
 			}
+		}	
 	}
-class MainOddEven
+class counting
 	{
+		int count;
+		public synchronized void countincrement()
+		{
+			count++;
+		}
+		public int getCount()
+		{
+			return count;
+		}		
 		
 	}
+class countMain
+	{
+		public static void main(String args[])
+		{
+			counting c=new counting();
+			threadcount c1=new threadcount(c);
+			c1.setName("T1");
+			threadcount c2=new threadcount(c);
+			c2.setName("T2");
+			threadcount c3=new threadcount(c);
+			c3.setName("T3");
+			c1.start();
+			
+			c2.start();
+			c3.start();
+		
+			try
+			{
+				c1.join();
+				c2.join();
+				c3.join();	
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+			System.out.println("Total Count:"+c.getCount());
+		}
+	}
+//--------------------------------------------------------------------------------------------------------
+class ATM
+	{
+	private static int balance=5000;
+	public synchronized boolean checkPin(int pin)
+	{
+		System.out.println("Checking Pin....!");
+		if(pin==1234)		
+		{
+			System.out.println("Pin Correct...");
+			return true;
+		}
+		else
+		{
+			System.out.println("Invalid Pin...");
+			return false;
+		}
+	}
+	
+	public static void withdraw(int amount)
+	{
+		System.out.println("Processing Withdraw....!");
+		if(balance>=amount)
+		{
+			balance-=amount;
+			System.out.println("Withdrawal Successful.....!\nAmount: "+amount);
+		}
+		else
+		{
+			System.out.println("Insufficient Amount");
+		}
+		
+	}
+
+
+	public synchronized void checkBalance()
+	{
+		System.out.println("Your Balance:"+balance);
+	}
+	public synchronized void printReceipt()
+	{
+		System.out.println("Printing Receipt Transaction Completed...!");
+	}
+		
+	}
+class Atmdemo
+	{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		ATM atm=new ATM();
+		System.out.println("Enter Your Pin:");
+		int atmpin=s.nextInt();
+		Thread t1=new Thread(()->{
+			if(atm.checkPin(atmpin))
+		{
+			//System.out.println("1.Withdraw\n2.CheckBalance");
+			//int option=s.nextInt();
+			//if(option==1)
+			//{
+			//System.out.println("Enter the amount to withdraw:");
+			//int amount=s.nextInt();
+			Thread t2=new Thread(()->atm.withdraw(2000));
+			//}
+			//if(option==2)
+			//{
+			Thread t3=new Thread(()->atm.checkBalance());
+			//}
+			Thread t4=new Thread(()->atm.printReceipt());
+			t2.start();
+			try
+			{
+			t2.join();
+			}
+			
+			catch(Exception e){}
+			
+			t3.start();
+			try
+			{
+			t3.join();
+			}
+			catch(Exception e){}
+			t4.start();
+		}
+	});
+
+		t1.start();
+
+		
+	}
+	}
+//----------------------------------------------------------------------
+
+class SharedBuffer {
+    private Queue<Integer> buffer = new LinkedList<>();
+    private final int maxSize = 5;
+
+    public synchronized void produce(int item) throws InterruptedException {
+        while (buffer.size() == maxSize) {
+            System.out.println("Buffer is full. Producer is waiting...");
+            wait();
+        }
+        buffer.add(item);
+        System.out.println("Produced: " + item);
+        notify();
+    }
+
+    public synchronized int consume() throws InterruptedException {
+        while (buffer.isEmpty()) {
+            System.out.println("Buffer is empty. Consumer is waiting...");
+            wait();
+        }
+        int item = buffer.poll();
+        System.out.println("Consumed: " + item);
+        notify();
+        return item;
+    }
+}
+
+class Producer implements Runnable {
+    private SharedBuffer buffer;
+
+    public Producer(SharedBuffer buffer) {
+        this.buffer = buffer;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 1; i <= 10; i++) {
+                buffer.produce(i);
+                Thread.sleep(100);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
+
+class Consumer implements Runnable {
+    private SharedBuffer buffer;
+
+    public Consumer(SharedBuffer buffer) {
+        this.buffer = buffer;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 1; i <= 10; i++) {
+                buffer.consume();
+                Thread.sleep(100); 
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
+
+ class ProducerConsumer {
+    public static void main(String[] args) {
+        SharedBuffer buffer = new SharedBuffer();
+        Producer producer = new Producer(buffer);
+        Consumer consumer = new Consumer(buffer);
+
+        Thread producerThread = new Thread(producer);
+        Thread consumerThread = new Thread(consumer);
+
+        producerThread.start();
+        consumerThread.start();
+    }
+}
